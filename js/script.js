08a7fd7e7cc49e0dabc8c7af8633ce298aee0d1f
Lucas gomes da cruz <lucaass1997rp@gmail.com>
2023-04-24 13:48:41 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const perguntas = document.querySelectorAll('.perguntas-dt dt');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls)
+
+
+    resposta.classList.toggle('ativo');
+    const ativa = resposta.classList.contains('ativo');
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
